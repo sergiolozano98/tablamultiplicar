ad0d6f0b64029d04e2912ab859b10947eb4e9a29
Sergio Lozano Bueno <selobu@floridauniversitaria.es>
2016-10-20 09:59:57 +0200

    while

---
+++ tablamulti.php
@@ -0,0 +1,15 @@
+<?php
+//asignamos un valor en una variable
+$num=1;
+// Realizamos una secuencia con el while.
+while ($num <= 10) {
+//almacenamos en la variable nueva la operacion
+  $operacion=$num * 2;
+//mostramos en pantalla la operacion
+  echo"2 * $num = $operacion <br>";
+//indicamos que la secuencia sea creciente
+  $num++;
+}
+
+
+ ?>
